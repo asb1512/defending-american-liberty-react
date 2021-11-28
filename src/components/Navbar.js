06a11181c0eb9b510cf78c933c7a0a34a9ec112e
asb1512 <andrew@olympicmedia.com>
2021-11-27 20:55:55 -0500

    added hamburger menu to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Navbar.css';
+
+import { Squash as Hamburger } from 'hamburger-react';
+
+
 
 export default function Navbar() {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="navbar">
-      
+
+      <div className="hamburger-cntr">
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+        />
+      </div>
     </div>
   )
 }
