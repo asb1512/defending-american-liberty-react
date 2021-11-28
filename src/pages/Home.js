2699f652dbbaa5083a1d7b787af7bbff03b1f149
asb1512 <andrew@olympicmedia.com>
2021-11-28 17:34:00 -0500

    added first couple parallax layers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React, { useRef } from 'react';
 import './Home.css';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import { useSpring, animated } from 'react-spring';
+// import { useSpring, animated } from 'react-spring';
 
-import bg from '../images/home/dal-bg.jpg';
+// import bg from '../images/home/dal-bg.jpg';
 
 
 
@@ -13,32 +13,37 @@ export default function Home() {
   const parallax = useRef(null);
 
   return (
-    // <div className="home-cntr">
 
-    //   <img
-    //     src={bg}
-    //     alt="Defending American Liberty"
-    //     className="home-bg"
-    //   />
+    <Parallax
+      ref={parallax}
+      pages={6}
+    >
 
-      <Parallax
-        ref={parallax}
-        pages={6}
-        enabled={false}
+      <ParallaxLayer
+        offset={0.25}
+        speed={2}
+        className="home-pg-1"
       >
+        <div className="dal-slogan">
+          TAKING <br />
+          <span className="dal-slogan-important">ACTION</span> <br />
+          TO DEFEND AMERICAN LIBERTY
+        </div>
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={0}
-          speed={2}
-        >
-          <div className="dal-slogan">
-            TAKING <br />
-            <span className="dal-slogan-important">ACTION</span> <br />
-            TO DEFEND AMERICAN LIBERTY
+      <ParallaxLayer
+        offset={1.25}
+        speed={1}
+      >
+        <div className="home-content-cntr">
+          <div className="home-content-title">Issue Ad Campaigns</div>
+
+          <div className="home-content">
+            Defending American Liberty develops targeted issue ad campaigns to support conservative elected officials to promote the America First agenda, defend American liberty, and the Constitution of the United States.
           </div>
-        </ParallaxLayer>
+        </div>
+      </ParallaxLayer>
 
-      </Parallax>
-    // </div>
+    </Parallax>
   )
 }
