267644051ab721629a4e274bf0ffa07ee04c2efb
asb1512 <andrew@olympicmedia.com>
2021-11-29 09:34:19 -0500

    adding pages 2 and 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ import './Home.css';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 // import { useSpring, animated } from 'react-spring';
 
-// import bg from '../images/home/dal-bg.jpg';
+import bg from '../images/home/dal-bg.jpg';
+import dc from '../images/home/dc-at-night.png'
+import mic from '../images/home/microphone.jpg';
 
 
 
@@ -17,7 +19,21 @@ export default function Home() {
     <Parallax
       ref={parallax}
       pages={6}
+      enabled={true}
     >
+
+
+      {/* PAGE 1 */}
+      <ParallaxLayer
+        offset={0}
+        speed={0}
+      >
+        <img
+          src={bg}
+          alt="Defending American Liberty"
+          className="dal-hero-img"
+        />
+      </ParallaxLayer>
 
       <ParallaxLayer
         offset={0.25}
@@ -31,17 +47,42 @@ export default function Home() {
         </div>
       </ParallaxLayer>
 
+
+      {/* PAGE 2 */}
+      <ParallaxLayer
+        offset={1}
+        speed={0}
+      >
+        <img
+          src={dc}
+          alt="Defending American Liberty: Issue Ad Campaigns"
+          className="home-page-2-img"
+        />
+      </ParallaxLayer>
+
+
       <ParallaxLayer
         offset={1.25}
         speed={1}
       >
         <div className="home-content-cntr">
-          <div className="home-content-title">Issue Ad Campaigns</div>
+          <div className="home-content-title dal-slogan text-shadow">Issue Ad Campaigns</div>
 
           <div className="home-content">
-            Defending American Liberty develops targeted issue ad campaigns to support conservative elected officials to promote the America First agenda, defend American liberty, and the Constitution of the United States.
+            Defending American Liberty develops targeted, issue-ad campaigns to support conservative elected officials to promote the America First agenda, defend American liberty, and the Constitution of the United States.
           </div>
         </div>
+      </ParallaxLayer>
+
+      {/* PAGE 3 */}
+      <ParallaxLayer
+        offset={2}
+        speed={0}
+      >
+        <img
+          src={mic}
+          alt="Defending American Liberty: Calls to Congress"
+        />
       </ParallaxLayer>
 
     </Parallax>
