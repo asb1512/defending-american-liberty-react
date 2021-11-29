6d6e67dd52f8245ca1dbdeee02eac085e92de0ab
asb1512 <andrew@olympicmedia.com>
2021-11-29 09:34:30 -0500

    made nav options all caps

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -4,18 +4,18 @@ import { useSpring, animated } from 'react-spring';
 export default function NavOptions(props) {
 
   const navOptionsStyle = useSpring({
-    height: props.toggled ? '50vh' : '0vh',
+    height: props.toggled ? '38vh' : '0vh',
   })
 
   return (
     <animated.div className="nav-options-cntr" style={navOptionsStyle}>
-      <ul>
-        <li>Achievements</li>
-        <li>DAL Chairman</li>
-        <li>Terms and Conditions</li>
-        <li>Privacy Policy</li>
-        <li>Contact Us</li>
-      </ul>
+      <div className="nav-options-list">
+        <div>ACHIEVEMENTS</div>
+        <div className="nav-option">DAL CHAIRMAN</div>
+        <div className="nav-option">TERMS & CONDITIONS</div>
+        <div className="nav-option">PRIVACY POLICY</div>
+        <div className="nav-option">CONTACT US</div>
+      </div>
     </animated.div>
   )
 }
